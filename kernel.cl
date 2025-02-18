fd46237ef9b8e5a0c62c15706f24a1cabd0b62de
ad656 <68047975+ad656@users.noreply.github.com>
2025-02-17 16:43:57 -0800

    Add files via upload

--- kernel.cl
+++ kernel.cl
@@ -1,89 +1,31 @@
-__kernel void matrixMultiply(
-    __global const int *restrict A,
-    __global const int *restrict B,
-    __global int *restrict C,
-    const unsigned int numARows,
-    const unsigned int numAColumns,
-    const unsigned int numBRows,
-    const unsigned int numBColumns,
-    const unsigned int numCRows,
-    const unsigned int numCColumns)
-{
-    #define TILE_SIZE 16
-    #define VECTOR_SIZE 4
-    #define PADDING 1
 
-    // Local memory tiles with padding to avoid bank conflicts
-    __local int Atile[TILE_SIZE][TILE_SIZE + PADDING];
-    __local int Btile[TILE_SIZE][TILE_SIZE + PADDING];
-
-    // Thread indices
-    const int globalRow = get_global_id(0);
-    const int globalCol = get_global_id(1);
-    const int localRow = get_local_id(0);
-    const int localCol = get_local_id(1);
-
-    // Pre-calculate global offsets
-    const int aRowOffset = globalRow * numAColumns;
-    const int bColOffset = globalCol;
-
-    // Use vector accumulator for better arithmetic intensity
-    int4 sum_vec = (int4)(0, 0, 0, 0);
-    
-    // Process tiles
-    const int numTiles = (numAColumns + TILE_SIZE - 1) / TILE_SIZE;
-    
-    for (int tile = 0; tile < numTiles; ++tile) {
-        const int tileOffset = tile * TILE_SIZE;
-        
-        // Vectorized load for matrix A
-        const int aCol = tileOffset + localCol;
-        if (globalRow < numARows && aCol < numAColumns) {
-            Atile[localRow][localCol] = A[aRowOffset + aCol];
-        } else {
-            Atile[localRow][localCol] = 0;
-        }
-        
-        // Vectorized load for matrix B with transpose
-        const int bRow = tileOffset + localRow;
-        if (bRow < numBRows && globalCol < numBColumns) {
-            Btile[localCol][localRow] = B[bRow * numBColumns + bColOffset];
-        } else {
-            Btile[localCol][localRow] = 0;
-        }
-        
-        barrier(CLK_LOCAL_MEM_FENCE);
-        
-        // Vectorized computation with loop unrolling
-        #pragma unroll 4
-        for (int k = 0; k < TILE_SIZE; k += VECTOR_SIZE) {
-            if (k + VECTOR_SIZE <= TILE_SIZE) {
-                int4 a_vec = (int4)(
-                    Atile[localRow][k],
-                    Atile[localRow][k+1],
-                    Atile[localRow][k+2],
-                    Atile[localRow][k+3]
-                );
-                
-                int4 b_vec = (int4)(
-                    Btile[localCol][k],
-                    Btile[localCol][k+1],
-                    Btile[localCol][k+2],
-                    Btile[localCol][k+3]
-                );
-                
-                sum_vec += a_vec * b_vec;
-            }
-        }
-        
-        barrier(CLK_LOCAL_MEM_FENCE);
-    }
-    
-    // Reduce vector sum
-    int final_sum = sum_vec.x + sum_vec.y + sum_vec.z + sum_vec.w;
-    
-    // Coalesced write to global memory
-    if (globalRow < numCRows && globalCol < numCColumns) {
-        C[globalRow * numCColumns + globalCol] = final_sum;
-    }
+__kernel void convolution2D(
+    __global int * inputData, __global int * outputData, __constant int * maskData,
+    int width, int height, int maskWidth,  int imageChannels, int stride){
+    //@@ Insert code to implement matrix multiplication here
+   
+    /**
+    maskWidth := 5
+    maskRadius := maskWidth/2 # this is integer division, so the result is 2
+    for i from 0 to height do
+        for j from 0 to width do
+            for k from 0 to channels
+                accum := 0
+                for y from -maskRadius to maskRadius do
+                    for x from -maskRadius to maskRadius do
+                        xOffset := j + x
+                        yOffset := i + y
+                        if xOffset >= 0 && xOffset < width &&
+                            yOffset >= 0 && yOffset < height then
+                            imagePixel := I[(yOffset * width + xOffset) * channels + k]
+                            maskValue := K[(y+maskRadius)*maskWidth+x+maskRadius]
+                            accum += imagePixel * maskValue
+                        end
+                    end
+                end
+                # pixels are in the range of 0 to 1
+                P[(i * width + j)*channels + k] = clamp(accum, 0, 1)
+            end
+        end
+    end**/
 }
--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <libgen.h>
+#include <string.h>
 
 #include "device.h"
 #include "kernel.h"
 #include "matrix.h"
+#include "img.h"
 
 #define CHECK_ERR(err, msg)                           \
     if (err != CL_SUCCESS)                            \
@@ -14,133 +18,68 @@
 
 #define KERNEL_PATH "kernel.cl"
 
-void OpenCLMatrixMultiply(Matrix *input0, Matrix *input1, Matrix *result)
+#define COMPUTE_OUTUT_DIM(input_dim, kernel_size, stride) \
+    ((input_dim - kernel_size) / stride + 1)
+
+void OpenCLConvolution2D(Image *input0, Matrix *input1, Image *result, int stride)
 {
-    // Load external OpenCL kernel code
-    char *kernel_source = OclLoadKernel(KERNEL_PATH); // Load kernel source
-
-    // Device input and output buffers
-    cl_mem device_a, device_b, device_c;
-
-    cl_int err;
-
-    cl_device_id device_id;    // device ID
-    cl_context context;        // context
-    cl_command_queue queue;    // command queue
-    cl_program program;        // program
-    cl_kernel kernel;          // kernel
-
-    // Find platforms and devices
-    OclPlatformProp *platforms = NULL;
-    cl_uint num_platforms;
-
-    err = OclFindPlatforms((const OclPlatformProp **)&platforms, &num_platforms);
-    CHECK_ERR(err, "OclFindPlatforms");
-
-    // Get the ID for the specified kind of device type.
-    err = OclGetDeviceWithFallback(&device_id, OCL_DEVICE_TYPE);
-    CHECK_ERR(err, "OclGetDeviceWithFallback");
-
-    // Create a context
-    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    CHECK_ERR(err, "clCreateContext");
-
-    // Create a command queue
-    queue = clCreateCommandQueueWithProperties(context, device_id, 0, &err);
-    CHECK_ERR(err, "clCreateCommandQueueWithProperties");
-
-    // Create the program from the source buffer
-    program = clCreateProgramWithSource(context, 1, (const char **)&kernel_source, NULL, &err);
-    CHECK_ERR(err, "clCreateProgramWithSource");
-
-    // Build the program executable
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    CHECK_ERR(err, "clBuildProgram");
-
-    // Create the compute kernel in the program we wish to run
-    kernel = clCreateKernel(program, "matrixMultiply", &err);
-    CHECK_ERR(err, "clCreateKernel");
+    // ... existing code ...
 
     //@@ Allocate GPU memory here
     device_a = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-                         sizeof(int) * input0->shape[0] * input0->shape[1], 
-                         NULL, &err);
+                             sizeof(int) * input0->shape[0] * input0->shape[1] * IMAGE_CHANNELS, 
+                             NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_a");
-
-    device_b = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-                         sizeof(int) * input1->shape[0] * input1->shape[1], 
-                         NULL, &err);
+    
+    device_b = clCreateBuffer(context, CL_MEM_READ_ONLY,
+                             sizeof(float) * input1->shape[0] * input1->shape[1],
+                             NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_b");
-
-    device_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-                         sizeof(int) * result->shape[0] * result->shape[1], 
-                         NULL, &err);
+    
+    device_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+                             sizeof(int) * result->shape[0] * result->shape[1] * IMAGE_CHANNELS,
+                             NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_c");
 
-
-
     //@@ Copy memory to the GPU here
-
     err = clEnqueueWriteBuffer(queue, device_a, CL_TRUE, 0,
-                          sizeof(int) * input0->shape[0] * input0->shape[1],
-                          input0->data, 0, NULL, NULL);
+                              sizeof(int) * input0->shape[0] * input0->shape[1] * IMAGE_CHANNELS,
+                              input0->data, 0, NULL, NULL);
     CHECK_ERR(err, "clEnqueueWriteBuffer device_a");
-
-    err = clEnqueueWriteBuffer(queue, device_b, CL_TRUE, 0,
-                          sizeof(int) * input1->shape[0] * input1->shape[1],
-                          input1->data, 0, NULL, NULL);
-    CHECK_ERR(err, "clEnqueueWriteBuffer device_b");
     
-    // Set the arguments to our compute kernel
-    // __global const int *A, __global const int *B, __global int *C,
-    // const unsigned int numARows, const unsigned int numAColumns,
-    // const unsigned int numBRows, const unsigned int numBColumns,
-    // const unsigned int numCRows, const unsigned int numCColumns
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_a);
-    CHECK_ERR(err, "clSetKernelArg 0");
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_b);
-    CHECK_ERR(err, "clSetKernelArg 1");
-    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_c);
-    CHECK_ERR(err, "clSetKernelArg 2");
-    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &input0->shape[0]);
-    CHECK_ERR(err, "clSetKernelArg 3");
-    err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &input0->shape[1]);
-    CHECK_ERR(err, "clSetKernelArg 4");
-    err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &input1->shape[0]);
-    CHECK_ERR(err, "clSetKernelArg 5");
-    err |= clSetKernelArg(kernel, 6, sizeof(unsigned int), &input1->shape[1]);
-    CHECK_ERR(err, "clSetKernelArg 6");
-    err |= clSetKernelArg(kernel, 7, sizeof(unsigned int), &result->shape[0]);
-    CHECK_ERR(err, "clSetKernelArg 7");
-    err |= clSetKernelArg(kernel, 8, sizeof(unsigned int), &result->shape[1]);
-    CHECK_ERR(err, "clSetKernelArg 8");
+    err = clEnqueueWriteBuffer(queue, device_b, CL_TRUE, 0,
+                              sizeof(float) * input1->shape[0] * input1->shape[1],
+                              input1->data, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueWriteBuffer device_b");
+
+    // ... existing code ...
 
     // @@ define local and global work sizes
-
-    size_t local_size[2] = {16, 16};  // 16x16 thread block
-    size_t global_size[2] = {
-    ((result->shape[0] + local_size[0] - 1) / local_size[0]) * local_size[0],
-    ((result->shape[1] + local_size[1] - 1) / local_size[1]) * local_size[1]
-};
-
+    size_t localWorkSize[2] = {16, 16};  // Typical work group size
+    size_t globalWorkSize[2] = {
+        ((result->shape[1] + localWorkSize[0] - 1) / localWorkSize[0]) * localWorkSize[0],
+        ((result->shape[0] + localWorkSize[1] - 1) / localWorkSize[1]) * localWorkSize[1]
+    };
+    
     //@@ Launch the GPU Kernel here
-
-     err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_size, local_size,
-                            0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
     CHECK_ERR(err, "clEnqueueNDRangeKernel");
-    clFinish(queue);
 
     //@@ Copy the GPU memory back to the CPU here
-
     err = clEnqueueReadBuffer(queue, device_c, CL_TRUE, 0,
-                         sizeof(int) * result->shape[0] * result->shape[1],
-                         result->data, 0, NULL, NULL);
-
+                             sizeof(int) * result->shape[0] * result->shape[1] * IMAGE_CHANNELS,
+                             result->data, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueReadBuffer device_c");
+    
     //@@ Free the GPU memory here
-
     clReleaseMemObject(device_a);
     clReleaseMemObject(device_b);
     clReleaseMemObject(device_c);
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+    free(kernel_source);
 }
 
 int main(int argc, char *argv[])
@@ -156,46 +95,51 @@ int main(int argc, char *argv[])
     const char *input_file_c = argv[3];
     const char *input_file_d = argv[4];
 
+    // get the dir from the input file
+    int stride;
+    char dir[256];
+    strcpy(dir, dirname(strdup(input_file_a))); 
+
     // Host input and output vectors and sizes
-    Matrix host_a, host_b, host_c, answer;
+    Image host_a, host_c, answer;
+    Matrix host_b;
     
     cl_int err;
 
-    err = LoadMatrix(input_file_a, &host_a);
-    CHECK_ERR(err, "LoadMatrix");
+    err = LoadImgRaw(input_file_a, &host_a);
+    CHECK_ERR(err, "LoadImg");
 
     err = LoadMatrix(input_file_b, &host_b);
     CHECK_ERR(err, "LoadMatrix");
 
-    err = LoadMatrix(input_file_c, &answer);
-    CHECK_ERR(err, "LoadMatrix");
+    // err = LoadImgTmp(input_file_c, &answer);
+    err = LoadImgRaw(input_file_c, &answer);
+    CHECK_ERR(err, "LoadImg");
+
+    // Load stride
+    err = LoadStride(dir, &stride);
+    CHECK_ERR(err, "LoadStride");
 
     int rows, cols;
     //@@ Update these values for the output rows and cols of the output
-
-    rows = host_a.shape[1];    
-    cols = host_b.shape[1];
-
-    //@@ Do not use the results from the answer matrix
-
+    //@@ Do not use the results from the answer image
     
+    rows = COMPUTE_OUTUT_DIM(host_a.shape[0], host_b.shape[0], stride);
+    cols = COMPUTE_OUTUT_DIM(host_a.shape[1], host_b.shape[0], stride);
 
     // Allocate the memory for the target.
     host_c.shape[0] = rows;
     host_c.shape[1] = cols;
-    host_c.data = (int *)malloc(sizeof(int) * host_c.shape[0] * host_c.shape[1]);
+    host_c.data = (int *)malloc(sizeof(int) * host_c.shape[0] * host_c.shape[1] * IMAGE_CHANNELS);
 
-    // Call your matrix multiply.
-    OpenCLMatrixMultiply(&host_a, &host_b, &host_c);
+    OpenCLConvolution2D(&host_a, &host_b, &host_c, stride);
 
-    // // Call to print the matrix
-    // PrintMatrix(&host_c);
+    // Save the image
+    SaveImg(input_file_d, &host_c);
 
-    // Save the matrix
-    SaveMatrix(input_file_d, &host_c);
-
-    // Check the result of the matrix multiply
-    CheckMatrix(&answer, &host_c);
+    // Check the result of the convolution
+    err = CheckImg(&answer, &host_c);
+    CHECK_ERR(err, "CheckImg");
 
     // Release host memory
     free(host_a.data);
