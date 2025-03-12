4291c233d3c02d85ac4b69b6e628941924399e5c
ad656 <68047975+ad656@users.noreply.github.com>
2025-03-11 22:59:19 -0700

    Update new-forward.cc

--- new-forward.cc
+++ new-forward.cc
@@ -16,16 +16,16 @@
         exit(EXIT_FAILURE);                            \
     }
 
-void OpenCLInterface::conv_forward_gemm_opencl_prolog(const float *host_y, const float *host_x, const float *host_k, cl_mem *device_y, cl_mem *device_x, cl_mem *device_k, cl_mem *device_x_unroll, const int B, const int M, const int C, const int H, const int W, const int K)
+void OpenCLInterface::conv_forward_gemm_opencl_prolog(
+    const float *host_y, const float *host_x, const float *host_k, 
+    cl_mem *device_y, cl_mem *device_x, cl_mem *device_k, cl_mem *device_x_unroll, 
+    const int B, const int M, const int C, const int H, const int W, const int K) 
 {
-    //@@ Allocate GPU memory here (don't forget batch sizes!)
-
-    //@@ Copy memory to the GPU here
-
     cl_int err;
     size_t x_size = sizeof(float) * B * C * H * W;
     size_t y_size = sizeof(float) * B * M * (H - K + 1) * (W - K + 1);
     size_t k_size = sizeof(float) * M * C * K * K;
+    size_t x_unroll_size = sizeof(float) * B * C * K * K * (H - K + 1) * (W - K + 1);
 
     *device_x = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, x_size, (void *)host_x, &err);
     CHECK_ERR(err, "clCreateBuffer device_x");
@@ -36,35 +36,29 @@ void OpenCLInterface::conv_forward_gemm_opencl_prolog(const float *host_y, const
     *device_y = clCreateBuffer(context, CL_MEM_WRITE_ONLY, y_size, NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_y");
 
+    *device_x_unroll = clCreateBuffer(context, CL_MEM_READ_WRITE, x_unroll_size, NULL, &err);
+    CHECK_ERR(err, "clCreateBuffer device_x_unroll");
 }
 
-void OpenCLInterface::conv_forward_gemm_opencl(cl_mem device_y, const cl_mem device_x, const cl_mem device_k, const cl_mem device_x_unroll, const int B, const int M, const int C, const int H, const int W, const int K)
+
+void OpenCLInterface::conv_forward_gemm_opencl(
+    cl_mem device_y, const cl_mem device_x, const cl_mem device_k, 
+    const cl_mem device_x_unroll, const int B, const int M, 
+    const int C, const int H, const int W, const int K) 
 {
-    //@@ ====== Start im2col =====
-
-    // @@ define local and global work sizes
-
-    //@@ Launch the im2col kernel here
-
-    //@@ ====== End im2col =====
-
-    //@@ ====== Start gemm =====
-
-    // @@ Call clblast::GemmBatched here
-
-    //@@ ====== End gemm =====
-
     cl_int err;
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     const int unrolled_width = H_out * W_out;
     const int unrolled_height = C * K * K;
 
-    size_t global_size[2] = {(size_t)M, (size_t)B * (size_t)H_out * (size_t)W_out};
-    size_t local_size[2] = {16, 16};
+    // Define global and local work sizes
+    size_t global_size_im2col[3] = {(size_t)B, (size_t)C, (size_t)(H * W)};
+    size_t local_size_im2col[3] = {1, 1, TILE_WIDTH}; // TILE_WIDTH = 16
 
-    err = clSetKernelArg(kernel_im2col, 0, sizeof(cl_mem), &device_x);
-    err |= clSetKernelArg(kernel_im2col, 1, sizeof(cl_mem), &device_x_unroll);
+    // Set Kernel Arguments for im2col
+    err = clSetKernelArg(kernel_im2col, 0, sizeof(cl_mem), &device_x_unroll);
+    err |= clSetKernelArg(kernel_im2col, 1, sizeof(cl_mem), &device_x);
     err |= clSetKernelArg(kernel_im2col, 2, sizeof(int), &B);
     err |= clSetKernelArg(kernel_im2col, 3, sizeof(int), &C);
     err |= clSetKernelArg(kernel_im2col, 4, sizeof(int), &H);
@@ -72,9 +66,11 @@ void OpenCLInterface::conv_forward_gemm_opencl(cl_mem device_y, const cl_mem dev
     err |= clSetKernelArg(kernel_im2col, 6, sizeof(int), &K);
     CHECK_ERR(err, "clSetKernelArg kernel_im2col");
 
-    err = clEnqueueNDRangeKernel(queue, kernel_im2col, 2, NULL, global_size, local_size, 0, NULL, NULL);
+    // Launch im2col kernel
+    err = clEnqueueNDRangeKernel(queue, kernel_im2col, 3, NULL, global_size_im2col, local_size_im2col, 0, NULL, NULL);
     CHECK_ERR(err, "clEnqueueNDRangeKernel im2col");
 
+    // GEMM operation using clBLAST
     clblast::GemmBatched<float>(
         clblast::Layout::kRowMajor, clblast::Transpose::kNo, clblast::Transpose::kNo,
         M, unrolled_width, unrolled_height,
@@ -86,12 +82,11 @@ void OpenCLInterface::conv_forward_gemm_opencl(cl_mem device_y, const cl_mem dev
         B, &queue, nullptr);
 }
 
-void OpenCLInterface::conv_forward_gemm_opencl_epilog(float *host_y, cl_mem device_y, cl_mem device_x, cl_mem device_k, cl_mem device_x_unroll, const int B, const int M, const int C, const int H, const int W, const int K)
+void OpenCLInterface::conv_forward_gemm_opencl_epilog(
+    float *host_y, cl_mem device_y, cl_mem device_x, cl_mem device_k, 
+    cl_mem device_x_unroll, const int B, const int M, 
+    const int C, const int H, const int W, const int K) 
 {
-    //@@ Copy the output back to host
-
-    //@@ Free the GPU memory here
-
     cl_int err;
     size_t y_size = sizeof(float) * B * M * (H - K + 1) * (W - K + 1);
     err = clEnqueueReadBuffer(queue, device_y, CL_TRUE, 0, y_size, host_y, 0, NULL, NULL);
