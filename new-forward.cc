27f8bcd541b4c29d17dae6b286b5ea1fd24a6041
ad656 <68047975+ad656@users.noreply.github.com>
2025-03-12 22:25:56 -0700

    Update new-forward.cc

--- new-forward.cc
+++ new-forward.cc
@@ -27,17 +27,23 @@ void OpenCLInterface::conv_forward_gemm_opencl_prolog(
     size_t k_size = sizeof(float) * M * C * K * K;
     size_t x_unroll_size = sizeof(float) * B * C * K * K * (H - K + 1) * (W - K + 1);
 
-    
-    *device_x = clCreateBuffer(this->opencl->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, x_size, (void *)host_x, &err);
+    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+    CHECK_ERR(err, "clCreateContext");
+
+    queue = clCreateCommandQueueWithProperties(context, device_id, 0, &err);
+    kernel = clCreateKernel(program, "im2col", &err);
+    program = clCreateProgramWithSource(context, 1, (const char **)&kernel_source, NULL, &err);
+    CHECK_ERR(err, "clCreateProgramWithSource");
+    *device_x = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, x_size, (void *)host_x, &err);
     CHECK_ERR(err, "clCreateBuffer device_x");
 
-    *device_k = clCreateBuffer(this->opencl->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, k_size, (void *)host_k, &err);
+    *device_k = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, k_size, (void *)host_k, &err);
     CHECK_ERR(err, "clCreateBuffer device_k");
 
-    *device_y = clCreateBuffer(this->opencl->context, CL_MEM_WRITE_ONLY, y_size, NULL, &err);
+    *device_y = clCreateBuffer(context, CL_MEM_WRITE_ONLY, y_size, NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_y");
 
-    *device_x_unroll = clCreateBuffer(this->opencl->context, CL_MEM_READ_WRITE, x_unroll_size, NULL, &err);
+    *device_x_unroll = clCreateBuffer(context, CL_MEM_READ_WRITE, x_unroll_size, NULL, &err);
     CHECK_ERR(err, "clCreateBuffer device_x_unroll");
 }
 
@@ -46,6 +52,8 @@ void OpenCLInterface::conv_forward_gemm_opencl(
     cl_mem device_y, const cl_mem device_x, const cl_mem device_k, 
     const cl_mem device_x_unroll, const int B, const int M, 
     const int C, const int H, const int W, const int K) 
+
+    
 {
     cl_int err;
     const int H_out = H - K + 1;
@@ -58,17 +66,17 @@ void OpenCLInterface::conv_forward_gemm_opencl(
     size_t local_size_im2col[3] = {1, 1, TILE_WIDTH}; // TILE_WIDTH = 16
 
     // Set Kernel Arguments for im2col
-    err = clSetKernelArg(this->opencl->kernel, 0, sizeof(cl_mem), &device_x_unroll);
-    err |= clSetKernelArg(this->opencl->kernel, 1, sizeof(cl_mem), &device_x);
-    err |= clSetKernelArg(this->opencl->kernel, 2, sizeof(int), &B);
-    err |= clSetKernelArg(this->opencl->kernel, 3, sizeof(int), &C);
-    err |= clSetKernelArg(this->opencl->kernel, 4, sizeof(int), &H);
-    err |= clSetKernelArg(this->opencl->kernel, 5, sizeof(int), &W);
-    err |= clSetKernelArg(this->opencl->kernel, 6, sizeof(int), &K);
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_x_unroll);
+    err |= clSetKernelArg(kernell, 1, sizeof(cl_mem), &device_x);
+    err |= clSetKernelArg(kernel, 2, sizeof(int), &B);
+    err |= clSetKernelArg(kernel, 3, sizeof(int), &C);
+    err |= clSetKernelArg(kernel, 4, sizeof(int), &H);
+    err |= clSetKernelArg(kernel, 5, sizeof(int), &W);
+    err |= clSetKernelArg(kernel, 6, sizeof(int), &K);
     CHECK_ERR(err, "clSetKernelArg kernel_im2col");
 
     // Launch im2col kernel
-    err = clEnqueueNDRangeKernel(this->opencl->queue,this->opencl->kernel, 3, NULL, global_size_im2col, local_size_im2col, 0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(queue, kernel, 3, NULL, global_size_im2col, local_size_im2col, 0, NULL, NULL);
     CHECK_ERR(err, "clEnqueueNDRangeKernel im2col");
 
     // GEMM operation using clBLAST
@@ -90,7 +98,7 @@ void OpenCLInterface::conv_forward_gemm_opencl_epilog(
 {
     cl_int err;
     size_t y_size = sizeof(float) * B * M * (H - K + 1) * (W - K + 1);
-    err = clEnqueueReadBuffer(this->opencl->queue, device_y, CL_TRUE, 0, y_size, host_y, 0, NULL, NULL);
+    err = clEnqueueReadBuffer(queue, device_y, CL_TRUE, 0, y_size, host_y, 0, NULL, NULL);
     CHECK_ERR(err, "clEnqueueReadBuffer device_y");
 
     clReleaseMemObject(device_x);
