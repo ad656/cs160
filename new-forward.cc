f305884e55db5b3ee70e5df9a8fd012a3e33d327
ad656 <68047975+ad656@users.noreply.github.com>
2025-03-13 18:29:11 -0700

    Update new-forward.cc

--- new-forward.cc
+++ new-forward.cc
@@ -55,7 +55,11 @@ void OpenCLInterface::conv_forward_gemm_opencl(
     const int W_out = W - K + 1;
 
     
-    size_t global_size_im2col[3] = {(size_t)W_out, (size_t)H_out, (size_t)(B*C)};
+    size_t global_size_im2col[3] = {
+        ((size_t)W_out + 15) / 16 * 16,  // Round up to multiple of 16
+        ((size_t)H_out + 15) / 16 * 16,  // Round up to multiple of 16
+        (size_t)(B*C)
+    };
     size_t local_size_im2col[3] = {16, 16, 1}; 
  
     // Set Kernel Arguments for im2col
@@ -120,7 +124,7 @@ void OpenCLInterface::conv_forward_gemm_opencl_epilog(
     CHECK_ERR(err, "clEnqueueReadBuffer device_y");
 
     clReleaseMemObject(device_x);
-    clReleaseMemObject(device_k);
+    clReleaseMemObject(device_k);.
     clReleaseMemObject(device_y);
     clReleaseMemObject(device_x_unroll);
 }
